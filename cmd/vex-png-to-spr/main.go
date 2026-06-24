4d83423b90b7d71b6645e02948adc9f1136c32ad
Peter Hellberg <peter@c7.se>
2026-06-24 02:29:34 +0200

    cmd: Add initial version of vex-png-to-spr tool

---
+++ cmd/vex-png-to-spr/main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"fmt"
+	"image"
+	"image/png"
+	"os"
+	"strings"
+)
+
+func main() {
+	if err := run(os.Args); err != nil {
+		os.Exit(1)
+	}
+}
+
+func run(args []string) error {
+	if len(args) < 2 {
+		fmt.Println("Usage: vex-png-to-spr image.png # Creates image.spr")
+	}
+
+	src := args[1]
+
+	base, found := strings.CutSuffix(src, ".png")
+	if !found {
+		return fmt.Errorf("input does not seem to be a .png")
+	}
+
+	f, err := os.Open(src)
+	if err != nil {
+		return fmt.Errorf("failed to open input: %w", err)
+	}
+	defer f.Close()
+
+	img, err := png.Decode(f)
+	if err != nil {
+		return fmt.Errorf("failed to decode png: %w", err)
+	}
+
+	pal, ok := img.(*image.Paletted)
+	if !ok {
+		return fmt.Errorf("error: PNG is not indexed (paletted).")
+	}
+
+	var (
+		data = pal.Pix
+		dx   = pal.Bounds().Dx()
+		dy   = pal.Bounds().Dy()
+		dst  = fmt.Sprintf("%s-%dx%d.spr", base, dx, dy)
+	)
+
+	out, err := os.Create(dst)
+	if err != nil {
+		return fmt.Errorf("failed to create output: %w", err)
+	}
+	defer out.Close()
+
+	_, err = out.Write(data)
+	if err != nil {
+		return fmt.Errorf("failed to write output: %w", err)
+	}
+
+	fmt.Printf("vex-png-to-spr: wrote %d bytes to %s\n", len(data), dst)
+
+	return nil
+}
