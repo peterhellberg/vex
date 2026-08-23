2993ba368b7f6e6a58cac974eb062fb64a217c90
Peter Hellberg <peter@c7.se>
2026-08-23 17:28:04 +0200

    cmd/vex: Use hex indices in the debug palette dump

--- cmd/vex/main.c
+++ cmd/vex/main.c
@@ -1035,7 +1035,7 @@ int main(int argc, char** argv) {
                changes, (unsigned long long)fnv1a64(g_fb, sizeof g_fb),
                (unsigned)m3_GetMemorySize(cart.rt));
         for (int i = 0; i < 16; i++)
-            printf("palette[%2d]=%02x%02x%02x\n", i,
+            printf("palette[%x]=%02x%02x%02x\n", i,
                    (unsigned)(g_palette[i] & 0xFF),
                    (unsigned)((g_palette[i] >> 8) & 0xFF),
                    (unsigned)((g_palette[i] >> 16) & 0xFF));
