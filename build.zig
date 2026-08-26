44d2d24846a36fd56724a4c24d445e80bbf75a4a
Peter Hellberg <peter@c7.se>
2026-08-26 22:19:32 +0200

    build: Add test_music

--- build.zig
+++ build.zig
@@ -65,7 +65,7 @@ pub fn build(b: *std.Build) void {
     // --- test carts: wasm32-freestanding stress-test modules ------------------
     const test_carts = [_][]const u8{
         "test_audio", "test_coords", "test_blit", "test_arith", "test_palette",
-        "test_api",   "test_bench",  "test_font", "test_hostile",
+        "test_api",   "test_bench",  "test_font", "test_hostile", "test_music",
     };
     inline for (test_carts) |name| {
         const t = b.addExecutable(.{
